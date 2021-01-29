6751dc776f9475ca67c2116f8a3bb489f7471a5b
Kevin Zhou <kevinzhou22@outlook.com>
2021-01-29 21:32:13 +1100

    Add fontawesome-free

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './css/reset.css';
+import '@fortawesome/fontawesome-free/css/solid.min.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
 
 function App() {
   return (
