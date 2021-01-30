5a7e0a268139b7209c2ac6a5fd70f50e3a134530
Kevin Zhou <kevinzhou22@outlook.com>
2021-01-30 23:03:31 +1100

    Add routing to App and import all necessary files for initial completion of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './css/reset.css';
 import '@fortawesome/fontawesome-free/css/solid.min.css';
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+import './css/general.css';
+import Header from './components/Header';
+import ItemPage from './components/ItemPage';
+import CartPage from './components/CartPage';
+import HomePage from './components/HomePage';
+import SuppliesPage from './components/SuppliesPage';
 
 function App() {
   return (
-    <div />
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/supplies" exact>
+          <SuppliesPage />
+        </Route>
+        <Route path="/supplies/:id" exact>
+          <ItemPage />
+        </Route>
+        <Route path="/cart" exact>
+          <CartPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
